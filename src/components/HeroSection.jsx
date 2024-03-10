e6dc2aeb5571d27f732befcb055a67c36fc4d650
VK-Study <88632111+VK-Study@users.noreply.github.com>
2024-03-11 04:22:32 +0530

    Changed favicon and title

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../images/logo.png'
+
+const HeroSection = () => {
+  return (
+    <div className='flex h-screen items-center justify-center'>
+      <img className='h-1/2' src={logo}/>
+    </div>
+  )
+}
+
+export default HeroSection
